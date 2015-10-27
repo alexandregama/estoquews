e096b0abd74e4ab0b53406d51055c2c4a6263d9b
alexandregama <alexandre.gama.lima@gmail.com>
2015-10-27 18:34:03 -0200

    Melhora o retorno de itens criando um Wrapper para a lista de itens

--- src/br/com/caelum/estoque/service/EstoqueWS.java
+++ src/br/com/caelum/estoque/service/EstoqueWS.java
@@ -10,6 +10,7 @@ import javax.jws.WebService;
 import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ListaItens;
 
 @WebService
 public class EstoqueWS {
@@ -35,6 +36,14 @@ public class EstoqueWS {
 		Item item = new ItemDao().quantidadeDo(codigo);
 		
 		return item;
+	}
+	
+	@WebMethod(operationName = "itensMelhores")
+	public ListaItens getListaDeItens() {
+		List<Item> lista = new ItemDao().todosItens();
+		ListaItens listaDeItens = new ListaItens(lista);
+		
+		return listaDeItens;
 	}
 	
 	@WebMethod(operationName = "itensPeloCodigo")
